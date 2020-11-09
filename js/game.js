e4cf829972dbc91ab5fb8c7dad0db84c0c55aeef
Franco De BAttista <cpn.francodebattista@gmail.com>
2020-11-09 11:34:24 -0300

    FDB-0004: 1.9 done

--- js/game.js
+++ js/game.js
@@ -25,7 +25,12 @@
         lastUpdate = 0,
         FPS = 0,
         frames = 0,
-        acumDelta = 0;
+        acumDelta = 0,
+        interval = 50,
+        CPS = 0,
+        cicles = 0,
+        x = 50,
+        y = 50;
 
 
 
@@ -156,6 +161,10 @@
         // Draw score
         ctx.fillText('Score: ' + score, 0, 10);
         
+        // Draw FPS
+        ctx.fillText('FPS: ' + FPS, 50, 10);
+        ctx.fillText('CPS: ' + CPS, 50, 20);
+
         // Draw pause
         if (pause) {
             ctx.textAlign = 'center';
@@ -166,18 +175,25 @@
             }
             ctx.textAlign = 'left';
         }
+
     }
 
+    
     function act() {
         var i;
         var l;
-
         if (!pause) {
+
+            x += 6;
+            if (x > canvas.width) {
+                x = 0;
+            }
+    
             // GameOver Reset
             if (gameover) {
                 reset();
             }
-            
+
             // Move Body
             for (i = body.length - 1; i > 0; i -= 1) {
                 body[i].x = body[i - 1].x;
@@ -267,28 +283,31 @@
 
     function repaint() {
         window.requestAnimationFrame(repaint);
+        frames++;
         paint(ctx);
     }
 
     function run() {
-        window.requestAnimationFrame(run);
-        
-        var now = Date.now(),
-        deltaTime = (now - lastUpdate) / 1000;
+        setTimeout(run, interval);
+
+        var now = Date.now();
+        var deltaTime = (now - lastUpdate) / 1000;
         if (deltaTime > 1) {
             deltaTime = 0;
         }
         lastUpdate = now;
-        frames += 1;
+
+        cicles += 1;
         acumDelta += deltaTime;
         if (acumDelta > 1) {
             FPS = frames;
+            CPS = cicles;
             frames = 0;
+            cicles = 0;
             acumDelta -= 1;
         }
 
         act();
-        paint(ctx);
     }
 
     function init() {
