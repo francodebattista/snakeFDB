eaa0aef41725fa8a67edf0d3c0b4a6dbe7779c7e
Franco De BAttista <cpn.francodebattista@gmail.com>
2020-11-09 11:49:45 -0300

    FDB-0005: 1.10 done

--- js/game.js
+++ js/game.js
@@ -53,6 +53,27 @@
         this.y = (y == null) ? 0 : y;
         this.width = (width == null) ? 0 : width;
         this.height = (height == null) ? this.width : height;
+
+        this.intersects = function (rect) {
+            if (rect == null) {
+                window.console.warn('Missing parameters on function intersects');
+            } else {
+                return (
+                    this.x < rect.x + rect.width &&
+                    this.x + this.width > rect.x &&
+                    this.y < rect.y + rect.height &&
+                    this.y + this.height > rect.y
+                );
+            }
+        };
+
+        this.fill = function (ctx) {
+            if (ctx == null) {
+                window.console.warn('Missing parameters on function fill');
+            } else {
+                ctx.fillRect(this.x, this.y, this.width, this.height);
+            }
+        };
     }
 
     Rectangle.prototype = {
